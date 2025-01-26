c9817dc8f2e8fb78c6fc0989b5ffb4c36a8fa17e
Vivek1819 <vivekhipparkar@gmail.com>
2025-01-26 14:33:52 +0530

    commits frontend done

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { useUser } from '@clerk/nextjs';
 import { ExternalLink, Github } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react'
+import CommitLog from './commit-log';
 
 const DashboardPage = () => {
   const { user } = useUser();
@@ -41,7 +42,7 @@ const DashboardPage = () => {
         </div>
       </div>
       <div className='mt-8'></div>
-      Commit log
+      <CommitLog />
     </div>
   )
 }
